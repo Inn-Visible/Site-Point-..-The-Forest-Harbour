c5f17b35bee12c5d840991acbe97f3a77e4d7e23
Quillinn C. Primrose <c.floralhoney@gmail.com>
2026-08-16 22:08:07 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='outflow-v26';
+const CACHE='outflow-v27';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(['donations.html','board.html']);}));
   self.skipWaiting();
