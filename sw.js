965443634d5b75d37d562e03398f23ff59e8f08f
Quillinn C. Primrose <c.floralhoney@gmail.com>
2026-09-22 12:07:33 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,9 @@ var TILES='hive-tiles-v1', KIT='hive-kit-v1';
 
 self.addEventListener('install',function(e){
   e.waitUntil(caches.open(KIT).then(function(c){
-    return c.addAll(['./kit.html']);
+    return Promise.all(['./kit.html','./donations.html','./board.html'].map(function(u){
+      return c.add(u).catch(function(){ return null; });
+    }));
   }));
   self.skipWaiting();
 });
